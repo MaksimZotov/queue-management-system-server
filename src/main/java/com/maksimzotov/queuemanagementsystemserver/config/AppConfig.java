a76f374bbab91458a871483887d337691248fa1a
MaksimZotov <zotovm256@gmail.com>
2023-02-18 00:40:49 +0300

    fix

--- src/main/java/com/maksimzotov/queuemanagementsystemserver/config/AppConfig.java
+++ src/main/java/com/maksimzotov/queuemanagementsystemserver/config/AppConfig.java
@@ -2,12 +2,14 @@ package com.maksimzotov.queuemanagementsystemserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableAsync
 public class AppConfig {
 
     @Bean
