f53573675a33be80b7db03ea97fb2a8a1c103a69
MaksimZotov <zotovm256@gmail.com>
2023-03-11 17:04:20 +0300

    in progress

--- src/main/java/com/maksimzotov/queuemanagementsystemserver/model/client/QueueStateForClient.java
+++ src/main/java/com/maksimzotov/queuemanagementsystemserver/model/client/QueueStateForClient.java
@@ -2,7 +2,6 @@ package com.maksimzotov.queuemanagementsystemserver.model.client;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.maksimzotov.queuemanagementsystemserver.entity.ClientEntity;
-import com.maksimzotov.queuemanagementsystemserver.model.queue.QueueStateModel;
 import lombok.Value;
 
 @Value
@@ -13,15 +12,13 @@ public class QueueStateForClient {
     Long locationId;
     String email;
     Integer code;
-    String status;
 
     public static QueueStateForClient toModel(ClientEntity clientEntity) {
         return new QueueStateForClient(
                 clientEntity.getId(),
                 clientEntity.getLocationId(),
                 clientEntity.getEmail(),
-                clientEntity.getCode(),
-                clientEntity.getStatus()
+                clientEntity.getCode()
         );
     }
 }
